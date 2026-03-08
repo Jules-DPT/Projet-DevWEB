531ef9fa503d3f4ae5c6f764f8e1feb74fc98341
boini <207913910+Arthur1984444@users.noreply.github.com>
2026-03-08 16:53:15 +0100

    V1 of postsrep gettotal, getposts, getpage, gettotalpage

---
+++ PROJET/assets/php/Postsrepository.php
@@ -0,0 +1,58 @@
+<?php
+
+class Postsrepository
+{
+    private $SQL;
+    private $limit=12;
+    private $total_page;
+    private $page;
+    private $offset;
+    public function __construct($SQL_,$page_,$limit_)
+    {
+        $this->SQL = $SQL_;
+        $this->page = $page_;
+        $this->limit = $limit_>=0?$limit_:$this->limit;
+        $this->offset = ($this->page - 1) * $this->limit;
+    }
+
+    private function setTotalPage($total_page_)
+    {
+        $this->totalPages=$total_page_<1?1:$total_page_;
+    }
+    public function CalcTotalPage()
+    {
+        $query ="SELECT COUNT(*) as total FROM posts";
+        $row=$this->SQL->query($query);
+        $total= $row->fetch_assoc();
+        $tot=ceil((int)$total['total'] / $this->limit);
+        $this->setTotalPage($tot);
+    }
+    public function getTotalPage()
+    {
+        return $this->total_page;
+    }
+
+    public function getPage()
+    {
+        return $this->page;
+    }
+    public function getPosts()
+    {
+        $query=("select posts.id AS id,titre, CONCAT(SUBSTRING_INDEX(description,' ',30), '...') AS description_pointille,remuneration,date,
+        nb_de_postulations from posts join bdd_web.date d on d.id = posts.id_date_post order by date LIMIT $this->limit OFFSET $this->offset");//rep
+        $row=$this->SQL->query($query);
+        $result=$row->fetch_assoc();
+        while($result) {
+            $posts[] = [
+                "id" => (int)$result['id'],
+                "titre" => htmlspecialchars($result['titre'], ENT_QUOTES, 'UTF-8'),
+                "description" => htmlspecialchars($result['description_pointille'], ENT_QUOTES, 'UTF-8'),
+                "remuneration" => htmlspecialchars($result['remuneration'], ENT_QUOTES, 'UTF-8'),
+                "nb_postulants" => (int)$result['nb_de_postulations'],
+                "nb_whishlist" => (int)$result['nombre_wishlist']
+            ];
+        }
+        return $posts;
+    }
+
+}
