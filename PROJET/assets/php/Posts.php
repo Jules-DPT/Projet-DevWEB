285020002e05c51e3b9b54eb8554ea588e63d800
boini <207913910+Arthur1984444@users.noreply.github.com>
2026-03-08 15:20:35 +0100

    creating Posts.php AKA model for Posts for an MVC approach

---
+++ PROJET/assets/php/Posts.php
@@ -0,0 +1,72 @@
+<?php
+
+class Posts
+{
+    private $id;
+    private $titre;
+    private $description;
+    private $date_creation;
+    private $email;
+    private $telephone;
+    private $adresse;
+    private $nb_postulations;
+    private $entreprise;
+    private $remuneration;
+    private $date_debut;
+    private $date_fin;
+    private $nb_whishlist;
+    private $contrat;
+    private $duree;
+
+    //---------------get-methods---------------------------------------------------------------------------------------
+    public function getId()
+    {
+    }
+    public function getTitre()
+    {
+    }
+
+    public function getDescription()
+    {
+    }
+    public function getDateCreation()
+    {
+    }
+    public function getEmail()
+    {
+    }
+    public function getTelephone()
+    {
+    }
+    public function getAdresse()
+    {
+    }
+    public function getNbPostulations()
+    {
+    }
+    public function getEntreprise()
+    {
+    }
+    public function getRemuneration()
+    {
+    }
+    public function getDateDebut()
+    {
+    }
+    public function getDateFin()
+    {
+    }
+    public function getNbWhishlist()
+    {
+    }
+    public function getContrat()
+    {
+    }
+    public function getDuree()
+    {
+    }
+    //---------------set-methods---------------------------------------------------------------------------------------
+    public function setTitre($titre_)
+    {
+    }
+}
