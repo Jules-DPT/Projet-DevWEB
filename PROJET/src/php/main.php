b3301d885ae47c78bc771e7560c29fda1e3763fe
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-03-10 16:27:08 +0100

    changing for an appropriate error catch (mysqli) + adding requirement for mysqli in composer.json

--- PROJET/src/php/main.php
+++ PROJET/src/php/main.php
@@ -1,12 +1,12 @@
 <?php
-
-
-
-
-
-$mysqli = new mysqli("172.19.80.1", "adm", "SUPERADMIN", "bdd_web", 3306);
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try {
+    $mysqli = new mysqli("172.19.80.1", "adm", "SUPERADMIN", "bdd_web", 3306);
+    $mysqli->set_charset("utf8mb4");
+} catch(Exception $e) {
+    error_log($e->getMessage());
+    exit('Error connecting to database');
 }
+
+
 
