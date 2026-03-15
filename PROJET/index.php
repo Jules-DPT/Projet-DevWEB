74a67cbe65faf88e31fa9be184c794554cd5badf
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-03-15 17:45:30 +0100

    new routing architecture

---
+++ PROJET/index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This is the router, the main entry point of the application.
+ * It handles the routing and dispatches requests to the appropriate controller methods.
+ */
+
+require "vendor/autoload.php";
+use App\php\Controllers\Recherchecontroller;
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+$loader = new \Twig\Loader\FilesystemLoader('src/Templates');
+$twig = new \Twig\Environment($loader, [
+    'debug' => true
+]);
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($uri) {
+    case '/':
+        // TODO : call the welcomePage method of the controller
+        //$controller->welcomePage();
+        //echo $twig->render('.html');
+        break;
+    case '/recherche':
+        $Page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $recherche = isset($_GET['recherche']) ? (string)$_GET['recherche'] : '';
+        $int =isset($_GET['type']) ? (int)$_GET['type'] : 3;
+        $recherchecontroller = new Recherchecontroller($int,$Page,$recherche,$twig);
+
+        $recherchecontroller->getPrimaryData();
+        break;
+
+    default:
+        // TODO : return a 404 error
+        echo '404 Not Found';
+        break;
+}
+
