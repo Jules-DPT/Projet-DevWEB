46a0184465cad260a4d73c4736b7b183c59235a5
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-03-20 15:29:56 +0100

    in final version

--- PROJET/src/php/main.php
+++ PROJET/src/php/main.php
@@ -1,14 +1,16 @@
 <?php
 namespace App\php;
-require "SQLconnect.php";
 
+
+use mysql_xdevapi\Exception;
 use mysqli;
-class main extends SQLconnect{
+class main  {
     private $mysqli;
     public function __construct(){
+        $env=parse_ini_file("../.env");
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         try {
-            $this->mysqli = new mysqli($this->host, $this->user, $this->mdp, $this->db, $this->port);
+            $this->mysqli = new mysqli($env["DB_HOST"], $env["DB_USER"], $env["DB_MDP"], $env["DB_BDD"], $env["DB_PORT"]);
             $this->mysqli->set_charset("utf8mb4");
         } catch(Exception $e) {
             error_log($e->getMessage());
