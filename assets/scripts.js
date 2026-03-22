580bdf14fa941c82637adb29a4dcfe5107eaead2
juli-mak <wafocassandra@yahoo.com>
2026-03-22 21:38:12 +0100

    Refactor frontend: remove old PROJET structure and add new HTML pages

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,3 +5,25 @@ const menuHamburger = document.querySelector(".menu-hamburger")
         navLinks.classList.toggle('mobile-menu')
         document.body.classList.toggle('no-scroll');
         })
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+document.body.classList.toggle("dark-mode");
+
+if(document.body.classList.contains("dark-mode")){
+localStorage.setItem("theme","dark");
+themeToggle.textContent="☀️";
+}else{
+localStorage.setItem("theme","light");
+themeToggle.textContent="🌙";
+}
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+const savedTheme = localStorage.getItem("theme");
+
+if(savedTheme === "dark"){
+document.body.classList.add("dark-mode");
+themeToggle.textContent="☀️";
+}
+});
