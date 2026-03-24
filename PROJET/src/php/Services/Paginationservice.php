1c3f48466d12117d6dc990bdcf00780e0b213481
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-03-24 16:04:55 +0100

    jsp

--- PROJET/src/php/Services/Paginationservice.php
+++ PROJET/src/php/Services/Paginationservice.php
@@ -6,15 +6,16 @@ use App\php\Services\Service;
 
 class Paginationservice extends Service
 {
-    public function __construct(){
-
+    public function __construct()
+    {
 
     }
     public function getPageData()
     {
     }
 
-    public function getPath($get_){
+    public function getPath($get_)
+    {
         $base=strtok($_SERVER['REQUEST_URI'], '?');
         $path=$base.$get_.'&page=';
         return $path;
