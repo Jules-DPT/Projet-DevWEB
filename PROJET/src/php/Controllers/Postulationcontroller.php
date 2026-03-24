9b6cd05d118bfb3abe211799c8250cc3aa5d4266
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-03-24 16:22:52 +0100

    check posts

---
+++ PROJET/src/php/Controllers/Postulationcontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\php\Controllers;
+
+use App\php\Controllers\Controller;
+
+class Postulationcontroller extends Controller
+{
+
+    protected function getPageData()
+    {
+        // TODO: Implement getPrimaryData() method.
+    }
+}
