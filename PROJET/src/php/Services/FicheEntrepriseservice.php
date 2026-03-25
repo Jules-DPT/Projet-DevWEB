05d30ea10a12e454cd09bfa3cdbf8d02e74c7edd
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-03-25 21:48:52 +0100

    supp of methods they went in fiche

--- PROJET/src/php/Services/FicheEntrepriseservice.php
+++ PROJET/src/php/Services/FicheEntrepriseservice.php
@@ -5,61 +5,35 @@ namespace App\php\Services;
 use App\php\Contenants\Commentaire;
 use App\php\Repositories\Entreprisesrepository;
 use App\php\Repositories\EvaluationEntreprisesrepository;
-use App\php\Services\Service;
+use App\php\Services\Fiche;
 
 
-class FicheEntrepriseservice extends Service
+class FicheEntrepriseservice extends Fiche
 {
-    private $id_entreprise;
-    private $Evaluationrepository;
 
-    private $role;
-    private $id_user;
+
     public function __construct($id_entreprise_,$role_,$id_user_)
     {
         $this->id_user=$id_user_;
         $this->role=$role_;
-        $this->id_entreprise = $id_entreprise_;
+        $this->id_cible = $id_entreprise_;
         $this->repository=new Entreprisesrepository();
         $this->Evaluationrepository=new EvaluationEntreprisesrepository();
     }
     public function getPageData()
     {
-        return $this->repository->getEntreprisebyid($this->id_entreprise);
+        return $this->repository->getEntreprisebyid($this->id_cible);
     }
 
     public function getCommentaire()
     {
-        return $this->Evaluationrepository->getDataById($this->id_entreprise);
+        return $this->getCommentaire();
     }
 
     public function setCommentaire($commentaire)
     {
-        $id=(int)$commentaire->getID();
-        $note=(float)$commentaire->getNote();
-        $com=trim((string)$commentaire->getCommentaire());
-        $id_user=$this->id_user;
-        $date=date('Y-m-d'); //automatic date
-        $id_cible=(int)$commentaire->getIdCible();
-        if ($id==null or $note==null or $com==null or $id_user==null or $date==null or $id_cible==null)
-        {
-            return false;
-        }
-        elseif($this->role=="ETUDIANT" or $this->role=="NO")
-        {
-            return false;
-        }
-        $com= new Commentaire(
-            $id,
-            $note,
-            $com,
-            "",
-            "",
-            $id_user,
-            $date,
-            $id_cible
-        );
-        return $this->Evaluationrepository->InsertData($com);
+        return $this->setCommentaire($commentaire);
     }
+
 
 }
