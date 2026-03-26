09597738bc806157d170d9a17af8a0283a4dedf9
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-03-26 20:44:47 +0100

    constructor overide + getcom like a search with pagination

--- PROJET/src/php/Repositories/EvaluationEntreprisesrepository.php
+++ PROJET/src/php/Repositories/EvaluationEntreprisesrepository.php
@@ -7,6 +7,28 @@ use App\php\Repositories\Repository;
 
 class EvaluationEntreprisesrepository extends Repository
 {
+    private $limit;
+    private $offset;
+    private $page;
+
+    public function __construct()
+    {
+        $this->autoSQL();
+        $num=func_num_args();
+        switch ($num) {
+            case 0:
+                break;
+            case 2:
+                $this->__construct1(func_num_args(0),func_get_arg(1));
+                break;
+        }
+    }
+
+    private function __construct1($page_,$limit_){
+        $this->page=$page_;
+        $this->limit=$limit_;
+        $this->offset = ($this->page - 1) * $this->limit;
+    }
 
     public function DeleteDataByID($id_)
     {
@@ -57,6 +79,33 @@ class EvaluationEntreprisesrepository extends Repository
             $data['date'],
             $data['id_entreprise']
         );
+        $result->close();
+        return $commentaire;
+    }
+
+    public function getCommentaire($id_)
+    {
+        $query="select ee.id,note,commentaire,u.nom,u.prenom,u.id as id_user,date,ee.id_entreprise from evaluation_entreprise ee
+                left join bdd_web.utilisateur u on u.id = ee.id_utilisateur
+                left join bdd_web.date d on d.id = ee.id_date
+                where id_entreprise=? ORDER BY date ASC LIMIT ? OFFSET ? ";
+        $row =$this->SQL->prepare($query);
+        $row->bind_param("iii",$id_,$this->limit,$this->offset);
+        $row->execute();
+        $result = $row->get_result();
+        $commentaire=[];
+        while ($data = $result->fetch_assoc()) {
+            $commentaire[] = new Commentaire(
+                (int)$data['id'],
+                $data['note'],
+                $data['commentaire'],
+                $data['nom'],
+                $data['prenom'],
+                $data['id_user'],
+                $data['date'],
+                $data['id_post']
+            );
+        }
         $result->close();
         return $commentaire;
     }
--- PROJET/src/php/Repositories/EvaluationPostsrepository.php
+++ PROJET/src/php/Repositories/EvaluationPostsrepository.php
@@ -8,6 +8,29 @@ use App\php\Repositories\Repository;
 class EvaluationPostsrepository extends Repository
 {
 
+    private $limit;
+    private $offset;
+    private $page;
+
+    public function __construct()
+    {
+        $this->autoSQL();
+        $num=func_num_args();
+        switch ($num) {
+            case 0:
+                break;
+            case 2:
+                $this->__construct1(func_num_args(0),func_get_arg(1));
+                break;
+        }
+    }
+
+    private function __construct1($page_,$limit_){
+        $this->page=$page_;
+        $this->limit=$limit_;
+        $this->offset = ($this->page - 1) * $this->limit;
+    }
+
     protected function DeleteDataByID($id_)
     {
         // TODO: Implement DeleteDataByID() method.
@@ -20,15 +43,14 @@ class EvaluationPostsrepository extends Repository
 
     protected function InsertData($contenant_)
     {
-        $id=$contenant_->getId();
         $note=$contenant_->getNote();
         $date=$contenant_->getDate();
         $commentaire=$contenant_->getCommentaire();
         $id_user=$contenant_->getIdUser();
         $id_cible=$contenant_->getIdCible();
-        $query="Insert into evaluation_posts(id, note, commentaire, id_utilisateur, id_post, id_date) 
-                values(?,?,?,?,?,?)";
-        $this->SQL->bind_param("idsiis",$id,$note,$commentaire,$id_user,$id_cible,$date);
+        $query="Insert into evaluation_posts( note, commentaire, id_utilisateur, id_post, id_date) 
+                values(?,?,?,?,?)";
+        $this->SQL->bind_param("dsiis",$note,$commentaire,$id_user,$id_cible,$date);
         $row=$this->SQL->execute($query);
         $result=$row->get_result();
         if ($result->affected_rows ==1) {
@@ -41,23 +63,54 @@ class EvaluationPostsrepository extends Repository
 
     protected function getDataByID($id_)
     {
-        $query="select es.id,note,commentaire,u.nom,u.prenom,u.id as id_user,date,es.id_post from evaluation_posts es
+        $query="select es.id as id,note,commentaire,u.nom,u.prenom,u.id as id_user,date,es.id_post from evaluation_posts es
                 left join bdd_web.utilisateur u on u.id = es.id_utilisateur
                 left join bdd_web.date d on d.id = es.id_date
-                where id_post=? ";
+                where es.id=? ";
         $result = $this->ExecuteQueryByID($query,$id_);
-        $data = $result->fetch_assoc();
-        $commentaire= new Commentaire(
-            (int)$data['id'],
-            $data['note'],
-            $data['commentaire'],
-            $data['nom'],
-            $data['prenom'],
-            $data['id_user'],
-            $data['date'],
-            $data['id_post']
-        );
+        $commentaire=[];
+        while ($data = $result->fetch_assoc()) {
+            $commentaire = new Commentaire(
+                (int)$data['id'],
+                $data['note'],
+                $data['commentaire'],
+                $data['nom'],
+                $data['prenom'],
+                $data['id_user'],
+                $data['date'],
+                $data['id_post']
+            );
+        }
         $result->close();
         return $commentaire;
     }
+
+    public function getCommentaire($id_)
+    {
+        $query="select es.id,note,commentaire,u.nom,u.prenom,u.id as id_user,date,es.id_post from evaluation_posts es
+                left join bdd_web.utilisateur u on u.id = es.id_utilisateur
+                left join bdd_web.date d on d.id = es.id_date
+                where id_post=? ORDER BY date ASC LIMIT ? OFFSET ?  ";
+        $row =$this->SQL->prepare($query);
+        $row->bind_param("iii",$id_,$this->limit,$this->offset);
+        $row->execute();
+        $result = $row->get_result();
+        $commentaire=[];
+        while ($data = $result->fetch_assoc()) {
+            $commentaire[] = new Commentaire(
+                (int)$data['id'],
+                $data['note'],
+                $data['commentaire'],
+                $data['nom'],
+                $data['prenom'],
+                $data['id_user'],
+                $data['date'],
+                $data['id_post']
+            );
+        }
+        $result->close();
+        return $commentaire;
+    }
+
+
 }
