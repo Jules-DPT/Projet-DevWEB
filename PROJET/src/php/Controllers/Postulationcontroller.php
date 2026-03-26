d87887f394542b0d72d5347a48fc2fd24627db69
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-03-26 20:48:00 +0100

    not complete need twig

--- PROJET/src/php/Controllers/Postulationcontroller.php
+++ PROJET/src/php/Controllers/Postulationcontroller.php
@@ -3,12 +3,30 @@
 namespace App\php\Controllers;
 
 use App\php\Controllers\Controller;
+use App\php\Services\Fileservice;
+use App\php\Services\Postulationservice;
 
 class Postulationcontroller extends Controller
 {
+    public function __construct($id_user_, $role_, $loggedin_,$file_,$id_post_)
+    {
+        $this->id_user = $id_user_;
+        $this->role = $role_;
+        $this->loggedin = $loggedin_;
+        $this->service=new Postulationservice($id_post_,$id_user_,$file_,"../../../../Files/utilisateur/CV/");
 
+
+    }
     protected function getPageData()
     {
-        // TODO: Implement getPrimaryData() method.
+
+        switch ($this->role) {
+            case "ETUDIANT":
+                echo $this->template->render('Postulation.html.twig');
+                break;
+            default:
+                echo $this->template->render('welcomepage.html.twig');
+                break;
+        }
     }
 }
