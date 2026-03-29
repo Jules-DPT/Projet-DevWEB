86a304c47f81cba2ebd175c949f5c67133d23aa8
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-03-29 15:17:45 +0200

    possibility to add coms in back at least

--- PROJET/src/php/Controllers/Fichecontroller.php
+++ PROJET/src/php/Controllers/Fichecontroller.php
@@ -15,7 +15,9 @@ class Fichecontroller extends Controller
     private $type;
     private $id_cible;
 
-    public function __construct($id_cible_,$page,$type,$id_user_,$role_,$template_)
+    private $Commentaire;
+
+    public function __construct($id_cible_,$page,$type,$id_user_,$role_,$Commentaire_,$template_)
     {
 
         $this->template=$template_;
@@ -25,14 +27,20 @@ class Fichecontroller extends Controller
         $this->type = $this->service->getType();
         $this->id_user=$this->service->getIdUser();
         $this->role=$this->service->getRole();
+        $this->Commentaire=trim((string)$Commentaire_);
+        if (strlen($this->Commentaire)!=0)
+        {
+            $this->service->setCommentaire($this->Commentaire);
+        }
     }
     public function getPageData()
     {
         $contenant=$this->service->getPageData();
         $totalpages=$this->service->getTotalPages();
         $path=$this->service->getPath("?id_cible=".$this->id_cible."&page=".$this->page);
+        $commentaires=$this->service->getCommentaire();
         header('Content-Type: text/html; charset=UTF-8');
-        echo $this->template->render('Fiche.html.twig',["path"=>$path,"totalPages"=>$totalpages,"contenant"=>$contenant,"type"=>$this->type,"id_cible"=>$this->id_cible,"role"=>$this->role]);
+        echo $this->template->render('Fiche.html.twig',["path"=>$path,"totalPages"=>$totalpages,"contenant"=>$contenant,"type"=>$this->type,"id_cible"=>$this->id_cible,"role"=>$this->role,"commentaires"=>$commentaires]);
 
     }
 }
