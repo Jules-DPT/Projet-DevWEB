2675ecfeebf0c2b8d0ed3529514d572734676e4d
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-03-29 14:34:41 +0200

    added the case fiche

--- PROJET/index.php
+++ PROJET/index.php
@@ -5,12 +5,14 @@
  */
 
 require "vendor/autoload.php";
+
+use App\php\Controllers\Fichecontroller;
 use App\php\Controllers\Recherchecontroller;
 
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+header('Content-Type: application/json');
 
 session_set_cookie_params([
     'lifetime' => 0, // expire à la fermeture du navigateur
@@ -49,6 +51,7 @@ switch ($uri) {
     case '/':
         // TODO : call the welcomePage method of the controller
         break;
+
     case '/recherche':
         $Page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
         $recherche = isset($_GET['recherche']) ? (string)$_GET['recherche'] : '';
@@ -58,9 +61,27 @@ switch ($uri) {
         $recherchecontroller->getPageData();
 
         break;
-    case '/Postuler':
-        echo "nope";
+
+    case '/recherche/fiche':
+        $id_cible=isset($_GET['id_cible']) ? (int)$_GET['id_cible'] : -1;
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $type =isset($_GET['type']) ? (int)$_GET['type'] : 3;
+        $fichecontroller = new Fichecontroller($id_cible,$page,$type,$id_user,$role,$twig);
+        $fichecontroller->getPageData();
         break;
+
+    case '/recherche/Fiche/Postuler':
+        echo "postuler";
+        break;
+
+    case '/connexion':
+        echo "connexion";
+        break;
+
+    case '/dashboard':
+        echo "dashboard";//je ne sais pas si nous en avons vraiment besoin ou si nous ferons via connexion ?
+        break;
+
     default:
         // TODO : return a 404 error
         echo '404 Not Found';
