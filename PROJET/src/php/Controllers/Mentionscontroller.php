69928a9869c06ded52859ed2698cd038dc259a1c
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-04-01 16:32:38 +0200

    ajout mentions legales

--- PROJET/src/php/Controllers/Mentionscontroller.php
+++ PROJET/src/php/Controllers/Mentionscontroller.php
@@ -3,6 +3,7 @@
 namespace App\php\Controllers;
 
 use App\php\Controllers\Controller;
+use App\php\Services\Paginationservice;
 
 class Mentionscontroller extends Controller
 {
@@ -10,9 +11,12 @@ class Mentionscontroller extends Controller
     public function __construct($template_)
     {
         $this->template=$template_;
+        $this->service=new Paginationservice();
     }
-    protected function getPageData()
+    public function getPageData()
     {
-        echo $this->template->render('Mentions-legales.html.twig');
+        $path=$this->service->getPath("");
+        header('Content-Type: text/html; charset=UTF-8');
+        echo $this->template->render('Mentions-legales.html.twig',["path"=>$path]);
     }
 }
