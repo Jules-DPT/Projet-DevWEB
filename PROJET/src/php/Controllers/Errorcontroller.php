dab869200bee102df100d99863d8643e8ae5c43a
arthur <207913910+Arthur1984444@users.noreply.github.com>
2026-04-02 17:06:40 +0200

    added role in construct

--- PROJET/src/php/Controllers/Errorcontroller.php
+++ PROJET/src/php/Controllers/Errorcontroller.php
@@ -7,14 +7,15 @@ use App\php\Controllers\Controller;
 class Errorcontroller extends Controller
 {
     private $error;
-    public function __construct($template_,$error_)
+    public function __construct($error_,$template_,$role_)
     {
         $this->template=$template_;
         $this->error=$error_;
+        $this->role=$role_;
     }
     public function getPageData()
     {
         header('Content-Type: text/html; charset=UTF-8');
-        echo $this->template->render('Error.html.twig',["error"=>$this->error]);
+        echo $this->template->render('Error.html.twig',["error"=>$this->error,"role"=>$this->role]);
     }
 }
